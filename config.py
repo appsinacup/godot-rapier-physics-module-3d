a781e228173cd16fad65bee14cedc730f589cdb2
Dragos Daian <daian.dragos@yahoo.com>
2025-11-09 12:13:39 +0100

    update. Add docs(draft)

--- config.py
+++ config.py
@@ -1,8 +1,30 @@
 def can_build(env, platform):
-    return False
     # Rapier doesn't support double precision
-    return env.get("precision", "single") != "double"
-
+    if env.get("precision", "single") == "double":
+        return False
+    return True
 
 def configure(env):
     pass
+
+def get_doc_classes():
+    return [
+        "Fluid3D",
+        "FluidEffect3D",
+        "FluidEffect3DElasticity",
+        "FluidEffect3DSurfaceTensionAKINCI",
+        "FluidEffect3DSurfaceTensionHE",
+        "FluidEffect3DSurfaceTensionWCSPH",
+        "FluidEffect3DViscosityArtificial",
+        "FluidEffect3DViscosityDFSPH",
+        "FluidEffect3DViscosityXSPH",
+        "RapierDirectBodyState3D",
+        "RapierDirectSpaceState3D",
+        "RapierMath3D",
+        "RapierPhysics3DExtensionLibrary",
+        "RapierPhysicsServer3D",
+        "RapierPhysicsServerFactory3D",
+    ]
+
+def get_doc_path():
+    return "doc_classes"
